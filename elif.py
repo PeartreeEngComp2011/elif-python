0154224b6980c756b47a12a454afaf9cd9482e8d
Daniel Gusmao <danielgp20@hotmail.com>
2022-03-30 11:23:10 -0300

    elif com python

---
+++ elif.py
@@ -0,0 +1,10 @@
+a = 5
+b = -2
+c = 9
+
+if (a>b and b<c):
+	print("Condicao 1 satisfeita!")
+elif (b<c or a>c):
+	print("Condicao 2 satisfeita!")
+elif (a>b or c>b):
+	print("Condicao 3 satisfeita!")
